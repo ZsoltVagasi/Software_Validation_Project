c73fc1a16ed3fb5fce3cae0ad00c09c74e5746d3
zozo <zoltan.szanto@ms.sapientia.ro>
2021-11-01 10:53:18 +0200

    Updated

--- relations_manager.py
+++ relations_manager.py
@@ -8,10 +8,10 @@ class RelationsManager:
             Employee(id=1, first_name="John", last_name="Doe", base_salary=3000,
                      birth_date=datetime.date(1970, 1, 31), hire_date=datetime.date(1990, 10, 1)),
 
-            Employee(id=2, first_name="Myrta", last_name="Tucson", base_salary=1000,
+            Employee(id=2, first_name="Myrta", last_name="Torkelson", base_salary=1000,
                      birth_date=datetime.date(1980, 1, 1), hire_date=datetime.date(2000, 1, 1)),
 
-            Employee(id=3, first_name="Jenny", last_name="Lynch", base_salary=1500,
+            Employee(id=3, first_name="Jettie", last_name="Lynch", base_salary=1500,
                      birth_date=datetime.date(1987, 1, 1), hire_date=datetime.date(2015, 1, 1)),
 
             Employee(id=4, first_name="Gretchen", last_name="Watford", base_salary=4000,
@@ -20,7 +20,7 @@ class RelationsManager:
             Employee(id=5, first_name="Tomas", last_name="Andre", base_salary=1600,
                      birth_date=datetime.date(1995, 1, 1), hire_date=datetime.date(2015, 1, 1)),
 
-            Employee(id=6, first_name="Scotty", last_name="Bilbo", base_salary=1300,
+            Employee(id=6, first_name="Scotty", last_name="Bomba", base_salary=1300,
                      birth_date=datetime.date(1977, 1, 1), hire_date=datetime.date(2008, 1, 1))
         ]
 
