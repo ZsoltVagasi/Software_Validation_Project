d5dbf77cf60d8876a431dc9ee03afee78790117f
Vágási Zsolt- Ferenc <zsoltvagasi6@gmail.com>
2023-02-28 19:54:36 +0200

    employee test correction

--- employee_test.py
+++ employee_test.py
@@ -8,4 +8,4 @@ def test_if_the_salary_of_an_employee_is_corect_PASS():
     test_employee = employee.Employee(id=9, first_name="NotJohn", last_name="NotDoe", base_salary=1000, birth_date=datetime.date(1970, 1, 31), hire_date=datetime.date(1998, 10, 10))
     test_employee_manager = employee_manager.EmployeeManager(test_relations)
     salary = test_employee_manager.calculate_salary(test_employee)
-    assert(salary == 3000)
+    assert(salary == 3500)
