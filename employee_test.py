7170d425233d1ac0595c77639fdae4db1a5c2882
Vágási Zsolt- Ferenc <zsoltvagasi6@gmail.com>
2023-03-14 18:45:55 +0200

    Test email send and calculate salary

--- employee_test.py
+++ employee_test.py
@@ -11,7 +11,7 @@ def test_if_the_salary_of_an_employee_is_corect_PASS():
     assert(salary == 3500)
     # assert(salary == 3000)
 
-def test_leader_salary():
+def test_leader_salary_PASS():
     test_relations = relations_manager.RelationsManager()
     test_employee = employee.Employee(id=9, first_name="NotJohn", last_name="NotDoe", base_salary=2000, birth_date=datetime.date(1970, 1, 31), hire_date=datetime.date(2008, 10, 10))
     test_employee_manager = employee_manager.EmployeeManager(test_relations)
@@ -23,3 +23,14 @@ def test_leader_salary():
     salary = test_employee_manager.calculate_salary(test_employee)
     assert(salary == 3900)
     # assert(salary == 3600)
+
+def test_calculate_salary_and_send_email_PASS(capfd):
+    test_relations = relations_manager.RelationsManager()
+    test_employee = employee.Employee(id=9, first_name="NotJohn", last_name="NotDoe", base_salary=2000, birth_date=datetime.date(1970, 1, 31), hire_date=datetime.date(2008, 10, 10))
+    test_employee_manager = employee_manager.EmployeeManager(test_relations)
+    test_relations.teams.update({9: [1, 2, 3]})
+    test_employee_manager.calculate_salary_and_send_email(test_employee)
+    captured = capfd.readouterr()
+    assert captured.out == "NotJohn NotDoe your salary: 4100 has been transferred to you.\n"
+    # assert captured.out == "NotJohn NotDoe your salary: 3600 has been transferred to you.\n"
+    
